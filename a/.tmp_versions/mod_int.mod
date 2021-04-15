adae5adc7a0e8a1999cb85b2071b890fbab478c8
Hrishikesh Vedantam <shikiikihs2@gmail.com>
2021-04-15 12:32:29 +0530

    outputs

--- a/.tmp_versions/mod_int.mod
+++ a/.tmp_versions/mod_int.mod
@@ -1,3 +1,3 @@
-/home/mininet/device_driver/a/mod_int.ko
-/home/mininet/device_driver/a/mod_int.o
+/home/hrishikesh/Desktop/IIITDM/Device_Drivers/Practice/DD_endsem_CED17I001/a/mod_int.ko
+/home/hrishikesh/Desktop/IIITDM/Device_Drivers/Practice/DD_endsem_CED17I001/a/mod_int.o
 
--- a/mod_int.mod.c
+++ a/mod_int.mod.c
@@ -16,6 +16,15 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 MODULE_INFO(retpoline, "Y");
 #endif
 
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xbc0f22cc, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x87bf2365, __VMLINUX_SYMBOL_STR(param_ops_int) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+};
+
 static const char __module_depends[]
 __used
 __attribute__((section(".modinfo"))) =
